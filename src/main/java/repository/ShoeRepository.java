ecccc9361f180742e1284c76f369e9f5741fe40b
IMHOJEONG <39ghwjd@naver.com>
2019-04-28 06:36:13 +0900

    Add ShoeRepository & ShoeRepository Interface

---
+++ src/main/java/repository/ShoeRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import domain.Shoe;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ShoeRepository implements ShoeRepositoryInterface{
+        private Collection<Shoe> shoes = new ArrayList<Shoe>();
+}
---
+++ src/main/java/repository/ShoeRepositoryInterface.java
@@ -0,0 +1,10 @@
+package repository;
+
+import domain.Shoe;
+
+public interface ShoeRepositoryInterface {
+    void save(Shoe shoe); // 임호정
+    Shoe findByLowPrice(); // 김신원
+    Shoe findByHighPrice(); // 김주성
+    Shoe findByTitle(); // 허규진
+}
