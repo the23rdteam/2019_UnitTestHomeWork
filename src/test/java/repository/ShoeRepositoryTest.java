93ce6cb16239caa5f988a43487294387c35e0e24
bluesocks <s42415466@gmail.com>
2019-05-02 20:28:29 +0900

    shoes를 shoeSpyList로 변경

--- src/test/java/repository/ShoeRepositoryTest.java
+++ src/test/java/repository/ShoeRepositoryTest.java
@@ -48,9 +48,9 @@ public class ShoeRepositoryTest {
         when(mockShoe.getLprice()).thenReturn(new Integer("2000"));
         assertTrue(mockShoe != null);
 
-        shoes.add(new Shoe("축구화","link","img", new Integer("15000"),new Integer("100000"),"다음", new Integer("3")));
-        assertThat(shoes.get(0).getLprice(), is(new Integer("15000")));
-        verify(shoes).get(0).getLprice();
+        shoeSpyList.add(new Shoe("축구화","link","img", new Integer("15000"),new Integer("100000"),"다음", new Integer("3")));
+        assertThat(shoeSpyList.get(0).getLprice(), is(new Integer("15000")));
+        verify(shoeSpyList).get(0).getLprice();
     }
 
     @Test
