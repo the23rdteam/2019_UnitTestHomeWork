53636fe45ff7d383abe8cc869f47f53b60fb946b
bluesocks <s42415466@gmail.com>
2019-05-02 18:21:33 +0900

    json-simple-1.1.1.jar 추가

--- src/main/java/api/APICall.java
+++ src/main/java/api/APICall.java
@@ -10,7 +10,6 @@ import repository.ShoeRepository;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
