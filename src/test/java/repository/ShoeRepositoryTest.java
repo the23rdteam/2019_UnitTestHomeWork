28f4f7f5782f4571bef0f0b75a05136ac4419cd7
bluesocks <s42415466@gmail.com>
2019-05-02 17:45:46 +0900

    shoes가 정말로 get(0) 함수와 getLprice() 함수를 호출하였는지 검증

--- src/test/java/repository/ShoeRepositoryTest.java
+++ src/test/java/repository/ShoeRepositoryTest.java
@@ -49,8 +49,7 @@ public class ShoeRepositoryTest {
 
         shoes.add(new Shoe("축구화","link","img", new Integer("15000"),new Integer("100000"),"다음", new Integer("3")))
         assertThat(shoes.get(0).getLprice(), is(new Integer("15000")));
-
-
+        verify(shoes).get(0).getLprice();
     }
 
     @Test
