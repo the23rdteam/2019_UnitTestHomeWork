f8f88829a44fc346b454969ed8ce4a123ea54ec8
bluesocks <s42415466@gmail.com>
2019-05-02 17:29:02 +0900

    잘못된 부분 수정

--- src/main/java/repository/ShoeRepository.java
+++ src/main/java/repository/ShoeRepository.java
@@ -19,11 +19,11 @@ public class ShoeRepository implements ShoeRepositoryInterface{
                 for (Shoe shoe: shoes) {
                         if (shoe.getLprice().equals(lowPrice)) {
                                 return shoe;
-                        } else {
-                                System.out.println("원하는 LowPrice " + lowPrice + "를 가지는 신발을 찾지못했습니다.");
-                                return null;
                         }
                 }
+
+                System.out.println("원하는 LowPrice " + lowPrice + "를 가지는 신발을 찾지못했습니다.");
+                return null;
         }
 
         public Shoe findByHighPrice(int highPrice){
